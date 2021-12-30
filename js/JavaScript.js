a14cc12699c7598cb37bfb2a56acb57f0048073e
mattplum <mattdouglee@gmail.com>
2021-12-29 21:26:06 -0500

    added cookie fortune

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,4 +1,16 @@
-﻿function openNav() {
+﻿var array = ["'Lee' means plum in Chinese", "I have a shih tzu named Bao"]
+
+
+$(document).ready(function(){
+    $('.fortune_cookie').on("click", function(e){
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+      alert(array[Math.floor(Math.random() * array.length)]);
+      console.log(Math.floor(Math.random() * array.length))
+    });
+  });
+
+function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 function closeNav() {
